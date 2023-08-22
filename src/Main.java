537347092659a73fa34ec50c0969b924e091450a
shopitha12 <142894857+shopitha12@users.noreply.github.com>
2023-08-22 16:35:12 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String username = "Admin";
+        String password = "Password";
+
+        Scanner check = new Scanner(System.in);
+        System.out.print("Enter Username: ");
+        String enteredUsername = check.nextLine();
+
+        System.out.print("Enter password: ");
+        String enteredPassword = check.nextLine();
+
+        check.close();
+
+        if(enteredUsername.equals(username)&& enteredPassword.equals(password)) {
+            System.out.println("Welcome " + username);
+        }
+        else {
+            System.out.println("ACCESS DENIED!");
+        }
+    }
 }
