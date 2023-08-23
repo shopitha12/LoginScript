9ed060a33cf2b3513c7af1163a2f8d5af7c28981
Shopitha <Shopitha@Shopithas-MacBook-Pro.local>
2023-08-23 13:00:39 +0100

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] usernames = {"Admin", "User1", "User2", "User3", "User4"};
+        String[] passwords = {"Password", "123456","qwerty", "abcdef", "secret"};
+        int maxAttempts = 3;
+        int attempts = 0;
+
+        Scanner check = new Scanner(System.in);
+        System.out.print("Enter Username: ");
+        String enteredUsername = check.nextLine();
+
+        int userIndex = -1;
+        for (int i = 0; i < usernames.length; i++) {
+            if(enteredUsername.equalsIgnoreCase(usernames[i])) {
+                userIndex = i;
+                break;
+            }
+        }
+
+        if (userIndex != -1) {
+            while (attempts < maxAttempts) {
+                System.out.print("Enter password: ");
+                String enteredPassword = check.nextLine();
+
+                if (enteredPassword.equals(passwords[userIndex])) {
+                    System.out.println("Welcome " + usernames[userIndex]);
+                    break;
+                } else {
+                    attempts++;
+                    System.out.println("Incorrect password!");
+                }
+            }
+        } else {
+            System.out.println("Username not found.");
+        }
+
+        if(attempts >= maxAttempts){
+            System.out.println("ACCESS DENIED!");
+        }
+
+        check.close();
+    }
 }
